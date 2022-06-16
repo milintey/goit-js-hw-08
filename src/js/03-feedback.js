9a8297fb082663954138e6a0b38eac83fb4e7368
Ilya Milinteev <ilyamilint@gmail.com>
2022-06-16 13:16:42 +0300

    comit 7

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,20 +4,28 @@ let formData = {};
 const FEEDBACK = "feedback-form-state";
 
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
 
-form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
 onSaveMessage();
+onProverka();
 
-function onFormInput (event) {
+function onProverka() {
+    if (localStorage.getItem(FEEDBACK)) {
+        const saveMessage = localStorage.getItem(FEEDBACK);
+        const save = JSON.parse(saveMessage);
+        formData = save;
+    }
+}
+
+function onFormInput(event) {
     formData[event.target.name] = event.target.value;
-
     const storage = JSON.stringify(formData);
-
     localStorage.setItem(FEEDBACK, storage);
+    
 }
 
 function onFormSubmit(event) {
@@ -32,10 +40,10 @@ function onFormSubmit(event) {
     }
 }
 
-function onSaveMessage() {
+function onSaveMessage() { 
     const saveMessage = localStorage.getItem(FEEDBACK);
     const save = JSON.parse(saveMessage);
-    
+
     if (save === null) {
         input.value = "";
         textarea.value = "";
@@ -45,8 +53,10 @@ function onSaveMessage() {
     } else if (save.email !== undefined && save.message === undefined) {
         input.value = save.email;
         textarea.value = "";
-    } else {
+    } else if (save) {
         input.value = save.email;
         textarea.value = save.message;
     }
 }
+
+
